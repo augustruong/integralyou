d6ece6e6a958c613e1bd54183ea7d37b613b0dde
vutuananh140697 <vutuananhsama@gmail.com>
2023-10-08 00:30:37 +0900

    crate and edit post use sharing component

--- react_app/src/pages/Admin/CreatePost.js
+++ react_app/src/pages/Admin/CreatePost.js
@@ -1,57 +1,14 @@
-import React, { useState, useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
-import { CKEditor } from '@ckeditor/ckeditor5-react'
+import { useNavigate } from "react-router-dom";
+import PostForm from "../../components/PostForm";
 import words from "../../words";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-const API_URL = process.env.REACT_APP_API_END_POINT;
-const UPLOAD_ENDPOINT = "file-upload";
-const GETFILE_ENDPOINT = "file-get";
-
-function uploadAdapter(loader) {
-    return {
-    upload: () => {
-        return new Promise((resolve, reject) => {
-        const body = new FormData();
-        loader.file.then((file) => {
-            body.append("file", file);
-            console.log(file)
-            // let headers = new Headers();
-            // headers.append("Origin", "http://localhost:3000");
-            fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
-            method: "post",
-            body: body
-            // mode: "no-cors"
-            })
-            .then((res) => res.json())
-            .then((res) => {
-                resolve({
-                default: `${API_URL}/${GETFILE_ENDPOINT}/${res.filename}`
-                });
-            })
-            .catch((err) => {
-                reject(err);
-            });
-        });
-        });
-    }
-    };
-}
-
-function uploadPlugin(editor) {
-    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-      return uploadAdapter(loader);
-    };
-}
 
 export default function CreatePost() {
     const navigate = useNavigate();
   
     const [inputs, setInputs] = useState([]);
     const [error, setError] = useState(null);
-    const [coverSrc, setCoverSrc] = useState(null);
 
     const hiddenFileInput = useRef(null);
 
@@ -64,11 +21,10 @@ export default function CreatePost() {
         const fileUploaded = event.target.files[0];
         const body = new FormData();
         body.append("file", fileUploaded);
-        fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+        fetch(`${words.api.admin.file.post}`, {
             method: "post",
             body: body
         }).then(() => {
-            setCoverSrc(words.api.admin.file.get(fileUploaded.name))
             setInputs(values => ({...values, ["cover"]: fileUploaded.name}));
         })
  
@@ -87,46 +43,15 @@ export default function CreatePost() {
       }
     
     return(
-        <div className="admin createNewPost">
-            {error ? <p>An error occurred: {error.message}</p> : null}
-                {/* <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> */}
-                <div style={{marginBottom:"var(--space-s)"}}>
-                    <Link to={`/admin/blogmanage`} >＜ Back to List</Link>
-                </div>
-                <h2 style={{marginBottom:"var(--space-xl)"}}>Create New Post</h2>
-                <form onSubmit={handleSubmit} className="flex-row gap-xl">
-                    <div>
-                        <div className="thumbnail"><img src={coverSrc}/></div>
-                        <button type="button" onClick={handleClick} style={{marginTop:"var(--space-s)"}}>
-                            Upload cover picture
-                            <input hidden accept="image/*" multiple type="file" ref={hiddenFileInput} onChange={handleCoverChange} />
-                        </button>
-                    </div>
-                    <div style={{width:"676px"}}>
-                        <div>
-                            <label style={{display:"inline-block",width:"100px"}}>Title</label>
-                            <input type="text" name="title" onChange={handleTextChange} style={{width:"calc(100% - 100px)",marginBottom:"var(--space-s)"}}/>
-                        </div>
-                        <div>
-                            <label style={{display:"inline-block",width:"100px"}}>Description</label>
-                            <input type="text" name="description" onChange={handleTextChange} style={{width:"calc(100% - 100px)",marginBottom:"var(--space-s)"}}/>
-                        </div>
-                    <CKEditor
-                        editor={ Editor }
-                        name="content"
-                        onChange={(event,editor) => {
-                            const data = editor.getData();
-                            console.log({event,editor,data});
-                            setInputs(values => ({...values, ["content"]: data}));
-                        }}
-                        config={{
-                            extraPlugins: [uploadPlugin]
-                          }}
-                        
-                    />
-                    <button type="submit" className="primary" name="add" style={{marginTop:"var(--space-base)"}}>Save</button>
-                    </div>
-                </form>
-        </div>
+        <PostForm
+            inputs={inputs}
+            setInputs={setInputs}
+            error={error}
+            hiddenFileInput={hiddenFileInput}
+            handleTextChange={handleTextChange}
+            handleCoverChange={handleCoverChange}
+            handleSubmit={handleSubmit}
+            handleClick={handleClick}
+        />
     )
 }
--- react_app/src/pages/Admin/EditPost.js
+++ react_app/src/pages/Admin/EditPost.js
@@ -1,53 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
-import { CKEditor } from '@ckeditor/ckeditor5-react'
+import PostForm from "../../components/PostForm";
 import words from "../../words";
-
-const API_URL = process.env.REACT_APP_API_END_POINT;
-const UPLOAD_ENDPOINT = "file-upload";
-const GETFILE_ENDPOINT = "file-get";
-
-function uploadAdapter(loader) {
-    return {
-    upload: () => {
-        return new Promise((resolve, reject) => {
-        const body = new FormData();
-        loader.file.then((file) => {
-            body.append("file", file);
-            console.log(file)
-            // let headers = new Headers();
-            // headers.append("Origin", "http://localhost:3000");
-            fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
-            method: "post",
-            body: body
-            // mode: "no-cors"
-            })
-            .then((res) => res.json())
-            .then((res) => {
-                resolve({
-                default: `${API_URL}/${GETFILE_ENDPOINT}/${res.filename}`
-                });
-            })
-            .catch((err) => {
-                reject(err);
-            });
-        });
-        });
-    }
-    };
-}
-
-function uploadPlugin(editor) {
-    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-      return uploadAdapter(loader);
-    };
-}
 
 export default function EditPost(){
     const navigate = useNavigate();
     const [inputs, setInputs] = useState([]);
+    const [error, setError] = useState(null);
     const {id} = useParams();
   
     useEffect(() => {
@@ -71,7 +31,7 @@ export default function EditPost(){
         const fileUploaded = event.target.files[0];
         const body = new FormData();
         body.append("file", fileUploaded);
-        fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+        fetch(`${words.api.admin.file.post}`, {
             method: "post",
             body: body
         }).then(() => {
@@ -86,46 +46,20 @@ export default function EditPost(){
         event.preventDefault();
         axios.put(words.api.admin.post.update(id), inputs).then(function(response){
             navigate('/admin/blogmanage');
-        });   
+        }).catch((error) => setError(error));   
     }
      
     return (
-    <div>
-        <div className="container h-100">
-        <div className="row">
-            <div className="col-2"></div>
-            <div className="col-8">
-                <h2>Edit Post</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <img src={words.api.admin.file.get(inputs.cover)}/>
-                        <label>Title</label>
-                        <input type="text" value={inputs.title} name="title" onChange={handleTextChange} />
-                        <button type="button" onClick={handleClick}>
-                            Upload cover picture
-                            <input hidden accept="image/*" multiple type="file" ref={hiddenFileInput} onChange={handleCoverChange} />
-                        </button>
-                        <label>Description</label>
-                        <input type="text" value={inputs.description} name="description" onChange={handleTextChange} />
-                    </div>
-                    <CKEditor
-                        editor={ Editor }
-                        name="content"
-                        data={inputs.content}
-                        onChange={(event,editor) => {
-                            const data = editor.getData();
-                            console.log({event,editor,data});
-                            setInputs(values => ({...values, ["content"]: data}));
-                        }}
-                        config={{
-                            extraPlugins: [uploadPlugin]
-                          }}
-                    />
-                    <button type="submit" name="add">Save</button>
-                </form>
-            </div>
-        </div>
-        </div>
-    </div>
+        <PostForm
+            inputs={inputs}
+            setInputs={setInputs}
+            error={error}
+            hiddenFileInput={hiddenFileInput}
+            handleTextChange={handleTextChange}
+            handleCoverChange={handleCoverChange}
+            handleSubmit={handleSubmit}
+            handleClick={handleClick}
+            isEdit
+        />
   );
 }
